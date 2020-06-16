9638b63ce7562ba86a20a2794c7b54c9247adfd8
Peter Choo <pchoo@centiq.co.uk>
2020-06-16 19:22:32 +0100

    Rename SCS NIC IP config

--- deploy/terraform/modules/app_tier/vm-scs.tf
+++ deploy/terraform/modules/app_tier/vm-scs.tf
@@ -7,7 +7,7 @@ resource "azurerm_network_interface" "scs" {
   enable_accelerated_networking = local.scs_nic_accelerated_networking
 
   ip_configuration {
-    name                          = "scs${count.index}-${var.application.sid}-nic-ip"
+    name                          = "IPConfig1"
     subnet_id                     = var.infrastructure.vnets.sap.subnet_app.is_existing ? data.azurerm_subnet.subnet-sap-app[0].id : azurerm_subnet.subnet-sap-app[0].id
     private_ip_address            = cidrhost(var.infrastructure.vnets.sap.subnet_app.prefix, tonumber(count.index) + local.ip_offsets.scs_vm)
     private_ip_address_allocation = "static"
